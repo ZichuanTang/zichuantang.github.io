68401696d3358710b75f86004ccc95656876f312
ZichuanTang <ZichuanTang@users.noreply.github.com>
2025-05-04 15:54:13 +0000

    deploy: 913a23e94eeb0a0fb3a9cf80a49765cf11e68d56

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746373644',resources: [
+    cacheName: 'chirpy-1746374052',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
