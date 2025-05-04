e50c3c93de53f339425e5cdda8d9b9bf7de33cab
ZichuanTang <ZichuanTang@users.noreply.github.com>
2025-05-04 15:10:12 +0000

    deploy: f003b4250adc1df2be05dd0627c67364cca08f66

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1746371411',resources: [
+      '/zichuantang/assets/css/jekyll-theme-chirpy.css',
+      '/zichuantang/',
+      
+        '/zichuantang/categories/',
+      
+        '/zichuantang/tags/',
+      
+        '/zichuantang/archives/',
+      
+        '/zichuantang/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
